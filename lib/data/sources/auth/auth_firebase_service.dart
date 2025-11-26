b35f7ca5827dc98282da787c7bbdc3d7a0ec44e4
Md. Tasnimul Hasan <114396737+mdtasnimulh@users.noreply.github.com>
2025-11-26 13:11:54 +0600

    uploading user data to firestore done

--- lib/data/sources/auth/auth_firebase_service.dart
+++ lib/data/sources/auth/auth_firebase_service.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flune/data/models/auth/create_user_req.dart';
@@ -36,9 +37,17 @@ class AuthFirebaseServiceImpl implements AuthFirebaseService {
   @override
   Future<Either> signUp(CreateUserReq createUserReq) async {
     try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      var data = await FirebaseAuth.instance.createUserWithEmailAndPassword(
           email: createUserReq.email,
           password: createUserReq.password
+      );
+
+      FirebaseFirestore.instance.collection('Users').add(
+        {
+          'fullName': createUserReq.fullName,
+          'email': data.user?.email,
+          'uid': data.user?.uid
+        }
       );
 
       return Right('Signup Was Successful');
